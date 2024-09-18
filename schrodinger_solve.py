9db85776eb475f75ce98ee0d8a37d393e7a9a498
Johannes Fricke <johannes.c.fricke@gmail.com>
2024-09-19 00:58:29 +0200

    Function for seperating the variables

--- schrodinger_solve.py
+++ schrodinger_solve.py
@@ -28,6 +28,31 @@ def import_input(directory="./"):
         input_file.close()
     return raw_input_data
 
+def save_variables(raw_input_data):
+    """
+    Seperates List into variables.
+
+    :param raw_input_data: Parameter from which to take the variables.
+
+    :return: Variables
+    """
+    m = float(raw_input_data[0])
+    line2 = raw_input_data[1].split(" ")
+    xMin = float(line2[0])
+    xMax = float(line2[1])
+    nPoint = int(line2[2])
+    line3 = raw_input_data[2].split(" ")
+    firstEV = int(line3[0])
+    lastEV = int(line3[1])
+    interp_type = raw_input_data[3]
+    num_interp_points = int(raw_input_data[4])
+    potential_points = []
+    for i in range(5, 5+num_interp_points):
+        potential_points.append(raw_input_data[i].split(" "))
+        potential_points[i-5][0] = float(potential_points[i-5][0])
+        potential_points[i-5][1] = float(potential_points[i-5][1])
+
+    return m, xMin, xMax, nPoint, firstEV, lastEV, interp_type, num_interp_points, potential_points
 
 raw_input_data = import_input("./")
-print(raw_input_data)
+m, xMin, xMax, nPoint, firstEV, lastEV, interp_type, num_interp_points, potential_points = save_variables(raw_input_data)
