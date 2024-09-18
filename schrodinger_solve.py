9f71978f236f4740078f7e0fcf2f9bbfac67598f
Johannes Fricke <johannes.c.fricke@gmail.com>
2024-09-19 00:02:18 +0200

    Created function to read input file

--- schrodinger_solve.py
+++ schrodinger_solve.py
@@ -4,5 +4,30 @@ import matplotlib as plt
 #import pytest 
 
 #read input file
-input_file = open(r"schrodinger.inp", "r")
-print(input_file)
+
+def import_input(directory="./"):
+    """
+    Returns lines from schrodinger.inp.
+    
+    :param directory: Optional parameter to set if input file is not located in root directory.
+
+    :return: List with fit parameters.
+    """
+    raw_input_data = []
+    try:
+        input_file = open(directory + "schrodinger.inp", "r")
+        
+    except FileNotFoundError:
+        print("Could not open {}".format(directory + "schrodinger.inp")) 
+
+    else:
+        for line in input_file:
+            line = line.split("\t")[0]
+            line = line.split("\n")[0]
+            raw_input_data.append(line)   
+        input_file.close()
+    return raw_input_data
+
+
+raw_input_data = import_input("./")
+print(raw_input_data)
