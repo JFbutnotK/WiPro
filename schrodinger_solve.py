c691b61c8ecfab65cca050badec03147c6448b78
Jasmin Reinhardt <jasmin.reinhardt1111@gmail.com>
2024-09-20 12:25:57 +0200

    Here I started implementing the function 'a'. This was done to start developing the equation solver for the Schroedinger equation. The next step will be to develop a way to describe the derivative delta^2.

--- schrodinger_solve.py
+++ schrodinger_solve.py
@@ -2,7 +2,22 @@ import numpy as np
 import scipy as sp
 import matplotlib as plt
 #import pytest 
+#solving the Schroedinger equation
 
-#read input file
-input_file = open(r"schrodinger.inp", "r")
-print(input_file)
+
+m = 1.0
+xMin = -2.0
+xMax = 2.0
+nPoint = 1999
+
+
+def a():
+    gitter = np.linspace(xMin, xMax, nPoint)
+    delta = 1
+    abkurzung = 1 / ( m * delta ** 2 )
+    return gitter
+
+
+
+print(a())
+
